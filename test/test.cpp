fbcfaaa2d49e99ba1beac53010fc0748435fb7c8
Mayank Pathak <33289342+mayankpathak10@users.noreply.github.com>
2018-09-22 17:55:52 -0400

    test edited

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,51 @@
-#include <gtest/gtest.h>
+/**
+ *  @file    test.cpp
+ *  @author  Mayank Pathak
+ *  @version 1.0
+ *  @copyright GNU Public License
+ *
+ *  @brief est-Driven Development exercise (with partner component)
+ *
+ *  @section DESCRIPTION
+ *
+ *  This is test file to declare the test cases for the PID controller
+ *
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include <memory>
+#include "lib.hpp"
+
+std::shared_ptr<Controller>
+    PID_Controller;  ///< Shared pointer for Controller class as PID_Controller
+
+/**
+ * @brief Class initialization test
+ */
+TEST(PIDTests, initializationTest) {
+  PID_Controller = std::make_shared<Controller>();
+
+  EXPECT_TRUE(PID_Controller->isInitialized());
+}
+
+/**
+ * @brief Class's get and set function test
+ */
+TEST(PIDTests, valuesTest) {
+  PID_Controller = std::make_shared<Controller>(0.7, 0.6, 0.32);
+
+  EXPECT_NEAR(PID_Controller->getkP(), 0.7, 0.00001);
+  EXPECT_NEAR(PID_Controller->getkI(), 0.6, 0.00001);
+  EXPECT_NEAR(PID_Controller->getkD(), 0.32, 0.00001);
+}
+
+/**
+ * @brief Test the convergence of logic
+ */
+TEST(PIDTests, computeTest) {
+  PID_Controller = std::make_shared<Controller>(0.7, 0.06, 0.32);
+
+  PID_Controller->compute(12.0);
+
+  EXPECT_NEAR(PID_Controller->returnState(), 11.9979, 0.0021);
 }
