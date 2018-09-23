4a080d01b1f7389a37f6e3667ac51815d7ad0233
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-09-23 15:52:20 -0400

    Remove unwanted headers

--- app/main.cpp
+++ app/main.cpp
@@ -8,14 +8,14 @@
  *
  * @section DESCRIPTION
  *
- *  This is the main file for the PID controller implementation.
+ * This is the main file for the PID controller implementation.
  *
  * @dependencies: This file needs "PID_controller.cpp" ,and
  *                "lib.hpp"
  */
+#include <iostream>
+
 #include "lib.hpp"
-#include "iostream"
-#include "memory"
 /**
  * @brief      Main function
  *
