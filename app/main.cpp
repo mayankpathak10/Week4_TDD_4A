beb7139c302abd25f642c7dc956be4c486919da6
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-09-23 17:39:33 -0400

    Add code to implemnt a basic PID controller using the Controller class

--- app/main.cpp
+++ app/main.cpp
@@ -14,7 +14,7 @@
  *                "lib.hpp"
  */
 #include <iostream>
-
+#include <cmath>
 #include "lib.hpp"
 /**
  * @brief      Main function
@@ -22,6 +22,29 @@
  * @return     return 0
  */
 int main() {
+
+  // Define initial and target states
+  double targetState = 10.0;
+  double initialState = 0.0;
+
+  // Define controller gain values
+  double kP = 0.1;
+  double kI = 0.5;
+  double kD = 0.01;
+
+  // Initialize PID controller object
+  Controller myPIDController(kP, kI, kD);
+
+  double currentState = initialState;
+  double error = targetState - currentState;
+
+
+  while (std::abs(error) > 0.01) {
+    myPIDController.compute(targetState, currentState);
+    currentState = myPIDController.returnState();
+    error = targetState - currentState;
+    std::cout << "The error now is: " << error << std::endl;
+  }
 
   return 0;
 }
