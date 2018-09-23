04faf706efa5de9d26ae53d30b32f11c54c697e7
Srinidhi Sreenath <sreenath.srinidhi@gmail.com>
2018-09-23 17:58:29 -0400

    Add comments and output stream for better clarity

--- app/main.cpp
+++ app/main.cpp
@@ -13,38 +13,56 @@
  * @dependencies: This file needs "PID_controller.cpp" ,and
  *                "lib.hpp"
  */
+
+// CPP Headers
 #include <iostream>
 #include <cmath>
+
+// Controller class
 #include "lib.hpp"
 /**
- * @brief      Main function
+ * @brief      Main function that implements Controller class
  *
  * @return     return 0
  */
 int main() {
 
   // Define initial and target states
-  double targetState = 10.0;
+  double targetState = 5.0;
   double initialState = 0.0;
+
+  std::cout << "The target velocity is " << targetState
+      << " and initial velocity is " << initialState << std::endl;
 
   // Define controller gain values
   double kP = 0.1;
   double kI = 0.5;
   double kD = 0.01;
 
+  std::cout << "The controller proportional, integral and derivative gains are "
+      << kP << " " << kI << " " << kD << " respectively" << std::endl;
+
   // Initialize PID controller object
   Controller myPIDController(kP, kI, kD);
 
-  double currentState = initialState;
-  double error = targetState - currentState;
+  // Define initial current state and initial error of the system
+  double currentState = initialState;  ///< Current state
+  double error = targetState - currentState;  ///< Initial error
 
-
+  // Compute the PID control outputs until error is less than 0.01
   while (std::abs(error) > 0.01) {
+    // Compute PID control output
     myPIDController.compute(targetState, currentState);
+    // Update the current state of the system
     currentState = myPIDController.returnState();
+    std::cout << "The updated velocity of the system is " << currentState
+        << std::endl;
+    // Update error of the system
     error = targetState - currentState;
-    std::cout << "The error now is: " << error << std::endl;
+    std::cout << "The system error is " << error << std::endl;
   }
+
+  std::cout << "PID controller process is complete!" << std::endl;
 
   return 0;
 }
